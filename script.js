63d62f1870b9baa6ad83ddc162585a33228431a1
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-23 02:51:24 +0000

    errors

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ document.getElementById('button').addEventListener('click', newfunction)
 function newfunction () {
   day = document.getElementById('input').value
   age = document.getElementById('input-2').value
-  if (day !== 'Monday' && day !== 'monday' && day !== 'Tuesday' && day !== 'tuesday' && day !== 'Wednesday' && day !== 'wednesday' && day !== 'Thursday' && day !== 'thursday' && day !== 'Friday' && day !== 'friday' && day !== 'Saturday' && day !== 'saturday' && day !== 'Sunday' && day !== 'sunday')
-  { document.getElementById('paragraph').innerHTML = 'Enter a proper day of the week'
-  } else if (age < 0)
-  { document.getElementById('paragraph').innerHTML = 'Enter a proper age'
+  if (day !== 'Monday' && day !== 'monday' && day !== 'Tuesday' && day !== 'tuesday' && day !== 'Wednesday' && day !== 'wednesday' && day !== 'Thursday' && day !== 'thursday' && day !== 'Friday' && day !== 'friday' && day !== 'Saturday' && day !== 'saturday' && day !== 'Sunday' && day !== 'sunday') { 
+    document.getElementById('paragraph').innerHTML = 'Enter a proper day of the week'
+  } else if (age < 0) { 
+    document.getElementById('paragraph').innerHTML = 'Enter a proper age'
   } else {
-    if (!(day === 'Saturday' || day === 'saturday' || day === 'Sunday' || day === 'sunday') && age >= 18)
-    { document.getElementById('paragraph').innerHTML = 'Time to go to work' 
+    if (!(day === 'Saturday' || day === 'saturday' || day === 'Sunday' || day === 'sunday') && age >= 18 { document.getElementById('paragraph').innerHTML = 'Time to go to work'
     } else if (!(day === 'Saturday' || day === 'saturday' || day === 'Sunday' || day === 'sunday') && age < 18)
     { document.getElementById('paragraph').innerHTML = 'Time for school'
     } else {
