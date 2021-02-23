0b998f5ee718f0103a6611252c73912ba910737a
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-23 03:22:03 +0000

    Final copy

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ document.getElementById('button').addEventListener('click', newfunction)
 function newfunction () {
   day = document.getElementById('input').value
   age = document.getElementById('input-2').value
-  if (day !== 'Monday' && day !== 'monday' && day !== 'Tuesday' && day !== 'tuesday' && day !== 'Wednesday' && day !== 'wednesday' && day !== 'Thursday' && day !== 'thursday' && day !== 'Friday' && day !== 'friday' && day !== 'Saturday' && day !== 'saturday' && day !== 'Sunday' && day !== 'sunday') { 
+  if (day !== 'Monday' && day !== 'monday' && day !== 'Tuesday' && day !== 'tuesday' && day !== 'Wednesday' && day !== 'wednesday' && day !== 'Thursday' && day !== 'thursday' && day !== 'Friday' && day !== 'friday' && day !== 'Saturday' && day !== 'saturday' && day !== 'Sunday' && day !== 'sunday') {
     document.getElementById('paragraph').innerHTML = 'Enter a proper day of the week'
-  } else if (age < 0) { 
+  } else if (age < 0) {
     document.getElementById('paragraph').innerHTML = 'Enter a proper age'
   } else {
-    if (!(day === 'Saturday' || day === 'saturday' || day === 'Sunday' || day === 'sunday') && age >= 18) 
-    { document.getElementById('paragraph').innerHTML = 'Time to go to work'
-    } else if (!(day === 'Saturday' || day === 'saturday' || day === 'Sunday' || day === 'sunday') && age < 18)
-    { document.getElementById('paragraph').innerHTML = 'Time for school'
+    if (!(day === 'Saturday' || day === 'saturday' || day === 'Sunday' || day === 'sunday') && age >= 18){
+      document.getElementById('paragraph').innerHTML = 'Time to go to work'
+    } else if (!(day === 'Saturday' || day === 'saturday' || day === 'Sunday' || day === 'sunday') && age < 18){
+      document.getElementById('paragraph').innerHTML = 'Time for school'
     } else {
       document.getElementById('paragraph').innerHTML = 'Time to relax for the weekend'
     }
